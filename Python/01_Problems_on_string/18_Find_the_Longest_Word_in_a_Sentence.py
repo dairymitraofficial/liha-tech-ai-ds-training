29d61d3733bde8310b550e70438f038bb6148d56
Aniruddha <dairymitra.official@gmail.com>
2026-08-23 20:16:31 +0530

    Add 18_Find_the_Longest_Word_in_a_Sentence.py

---
+++ Python/01_Problems_on_string/18_Find_the_Longest_Word_in_a_Sentence.py
@@ -0,0 +1,12 @@
+text = input("Enter a sentence: ")
+
+words = text.split()
+
+longest = ""
+
+for word in words:
+    if len(word) > len(longest):
+        longest = word
+
+print("Longest word =", longest)
+print("Length =", len(longest))
