9871b86a05ea89d8670444c3a635c90b63a682a8
Aniruddha <dairymitra.official@gmail.com>
2026-08-23 20:07:49 +0530

    Add 10_replace_word_in_sentence.py

---
+++ Python/01_Problems_on_string(1)/10_replace_word_in_sentence.py
@@ -0,0 +1,8 @@
+str = input("Enter a string- ")
+
+print("The given string - ", str)
+
+str2 = input("Enter a word to replace- ")
+str3 = input("Enter a word with replace- ")
+
+print(str.replace(str2, str3))
