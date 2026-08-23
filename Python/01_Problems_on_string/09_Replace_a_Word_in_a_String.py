dad185e1f2f49d10e78be13f81d3e4031c825113
Aniruddha <dairymitra.official@gmail.com>
2026-08-23 20:16:28 +0530

    Add 09_Replace_a_Word_in_a_String.py

---
+++ Python/01_Problems_on_string/09_Replace_a_Word_in_a_String.py
@@ -0,0 +1,7 @@
+sentence = input("Enter a sentence: ")
+old_word = input("Enter word to replace: ")
+new_word = input("Enter new word: ")
+
+result = sentence.replace(old_word, new_word)
+
+print("Updated sentence =", result)
